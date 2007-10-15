4ca290f022ccf0b55fb12e72079e56681ab9425e
bob <bob@a458aef6-cc11-11dd-8829-37334faa441c>
2007-10-15 23:15:48 +0000

    rename phase 1 com to org

---
+++ trunk/org.crap4j.eclipse/src/org/crap4j/crap4jeclipse/popup/actions/Crap4jAction.java
@@ -0,0 +1,42 @@
+package org.crap4j.crap4jeclipse.popup.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class Crap4jAction implements IObjectActionDelegate {
+
+	/**
+	 * Constructor for Action1.
+	 */
+	public Crap4jAction() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		Shell shell = new Shell();
+		MessageDialog.openInformation(
+			shell,
+			"Crap4jEclipse Plug-in",
+			"Run Crap4j was executed.");
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+}
