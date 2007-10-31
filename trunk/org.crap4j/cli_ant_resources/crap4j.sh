5779c7b87bc7a07f720f6785108c0a4019a26a13
bob <bob@a458aef6-cc11-11dd-8829-37334faa441c>
2007-10-31 20:39:31 +0000

    in process of getting command line version working

--- trunk/org.crap4j/cli_ant_resources/crap4j.sh
+++ trunk/org.crap4j/cli_ant_resources/crap4j.sh
@@ -4,4 +4,4 @@ DEFAULT_CRAP4J_HOME=`dirname "$0"`
 
 echo "Default crap4j home is $DEFAULT_CRAP4J_HOME"
 
-java -cp $DEFAULT_CRAP4J_HOME/lib/org.crap4j.jar:$DEFAULT_CRAP4J_HOME/lib/args4j-2.0.1.jar:$DEFAULT_CRAP4J_HOME/lib/asm-3.0.jar:$DEFAULT_CRAP4J_HOME/lib/asm-tree-3.0.jar:$DEFAULT_CRAP4J_HOME/lib/asm-analysis-3.0.jar:$DEFAULT_CRAP4J_HOME/lib/asm-commons-3.0.jar:$DEFAULT_CRAP4J_HOME/lib/asm-util-3.0.jar:$DEFAULT_CRAP4J_HOME/lib/asm-xml-3.0.jar:$DEFAULT_CRAP4J_HOME/lib/com.agitar.eclipse.coverage_4.2.0.401405/com.agitar.coverage.jar org.crap4j.Main $@
+java -cp $DEFAULT_CRAP4J_HOME/lib/org.crap4j.jar:$DEFAULT_CRAP4J_HOME/lib/args4j-2.0.1.jar:$DEFAULT_CRAP4J_HOME/lib/asmlib.jar:$DEFAULT_CRAP4J_HOME/lib/com.agitar.eclipse.coverage_4.2.0.401405/com.agitar.coverage.jar org.crap4j.Main $@
